6fc95f031059e21f4819dac88ffee74b5fb039e1
unknown <hrppanchal27@gmail.com>
2023-11-03 13:26:27 +0530

    Added Vercel with mongoDb Atlass

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const connectionWithAtlas = ()=>{
 
     mongoose
-    .connect(process.env.MONGODB_CONNECT_STRING_VERCEL , {
+    .connect(process.env.MONGODB_CONNECT_STRING , {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             // useCreateIndex: true,
