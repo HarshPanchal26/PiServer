606f747c64a6b6d3b05b9f4ce549c0cef2ab6027
unknown <hrppanchal27@gmail.com>
2023-11-15 10:01:30 +0530

    Lat try is done

--- middleware/middlewareForAuthentication.js
+++ middleware/middlewareForAuthentication.js
@@ -64,7 +64,7 @@ const isTokenValid = (req, res, next) => {
 
 // method for authenticate session token whethere it is valid or not 
 const isAutorized = (req, res, next) => {
-    console.log("headers is Authorized <==>", req);
+    console.log("headers is Authorized <==>", req.cookies);
     let token = req.cookies['access_token'];
     if (token) {
         try {
